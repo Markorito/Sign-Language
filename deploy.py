dbc4cee49404062ccaa34c56e474e32b03a82779
markorito <kitongamark@gmail.com>
2022-01-12 17:40:14 +0300

    commit

--- deploy.py
+++ deploy.py
@@ -5,7 +5,7 @@ from PIL import Image, ImageOps
 import streamlit as st
 
 #Loading in the model
-model = tf.keras.models.load_model('saved_model/sign-language/saved_model.pb')
+model = tf.keras.models.load_model('saved_model/sign-language')
 
 st.write("""
          # Simple Sign Language Hand Classification Tool
@@ -15,22 +15,48 @@ st.write("""
 
 file = st.file_uploader("Upload the image file", type=["jpg", "png"])
 
-def import_and_predict(image_data, model):
-    
-        size = (100,100)    
-        image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
-        image = np.asarray(image)
-        img_resize = (cv2.resize(img, dsize=(100, 100),    interpolation=cv2.INTER_CUBIC))/255.
-        
-        img_reshape = img_resize[np.newaxis,...]
-    
-        prediction = model.predict(img_reshape)
-        
-        return prediction
+def classification(img, model):
+
+
+
+    # Create the array of the right shape to feed into the keras model
+
+    data = np.ndarray(shape=(1, 100, 100, 3), dtype=np.float32)
+
+    image = img
+
+    #image sizing
+
+    size = (100, 100)
+
+    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+
+    #turn the image into a numpy array
+
+    image_array = np.asarray(image)
+
+    # Normalize the image
+
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+
+
+    # Load the image into the array
+
+    data[0] = normalized_image_array
+
+
+    # run the inference
+
+    prediction = model.predict(data)
+
+    return np.argmax(prediction) # return position of the highest probability
+
 if file is None:
     st.text("Please upload an image file")
 else:
     image = Image.open(file)
     st.image(image, use_column_width=True)
-    prediction = import_and_predict(image, model)
-    st.write(prediction)
+    predictions = classification(image, )
+    labels = ['A','B','C','D','del','E','F','G','H','I','J','K','L','M','N','nothing','O', 'P','Q','R','S','space','T','U','V','W','X','Y','Z']
+    st.write(labels[predictions])
